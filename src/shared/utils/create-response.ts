53c0986094454ff315920748968eaa8c93a66213
Abner Persio <binhopersio@gmail.com>
2024-04-05 17:49:48 -0300

    feat: create response util

---
+++ src/shared/utils/create-response.ts
@@ -0,0 +1,28 @@
+import { HttpResponse } from '~/infra/http/base';
+
+export class CreateResponse {
+  static ok(payload?: unknown): HttpResponse {
+    const field = typeof payload === 'string' ? 'message' : 'data';
+    return { status: 200, [field]: payload };
+  }
+
+  static created(data?: unknown): HttpResponse {
+    return { status: 201, data };
+  }
+
+  static noContent(): HttpResponse {
+    return { status: 204 };
+  }
+
+  static notFound(message?: string): HttpResponse {
+    return { status: 404, message: message || 'Not found' };
+  }
+
+  static forbidden(): HttpResponse {
+    return { status: 403 };
+  }
+
+  static unauthorized(message?: string): HttpResponse {
+    return { status: 401, message };
+  }
+}
