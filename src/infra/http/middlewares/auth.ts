da8dba07230f1be58e6ec24306b1c35d76083d61
Abner Persio <binhopersio@gmail.com>
2024-04-27 14:10:10 -0300

    feat: auth middleware

---
+++ src/infra/http/middlewares/auth.ts
@@ -0,0 +1,53 @@
+import { Request } from 'express';
+import { prisma } from '~/infra/config/database';
+import { PostgresUserRepository } from '~/infra/database/repositories/postgres/user';
+import { UserRepository } from '~/infra/database/repositories/user';
+import { ExpiredTokenError } from '~/infra/errors/expired-token';
+import { BaseMiddleware } from '~/infra/http/middlewares/base';
+import { CreateResponse } from '~/shared/utils/create-response';
+import { JWT } from '~/shared/utils/jwt';
+
+type UserPayload = {
+  id: string;
+};
+
+export class AuthMiddleware extends BaseMiddleware {
+  constructor(private readonly respository: UserRepository) {
+    super();
+  }
+
+  static create() {
+    const repo = new PostgresUserRepository(prisma);
+    return new AuthMiddleware(repo);
+  }
+
+  protected async execute(req: Request) {
+    const authorization = req.headers.authorization;
+    const [_, token] = authorization?.split('Bearer ') || [];
+
+    if (!token) {
+      return CreateResponse.unauthorized('Invalid token');
+    }
+
+    try {
+      const user = JWT.verify<UserPayload>(token);
+      if (!user?.id) return CreateResponse.unauthorized('Invalid token');
+
+      const hasUser = await this.respository.findById(user.id);
+      if (!hasUser) return CreateResponse.unauthorized('Invalid token');
+
+      req.context = { user };
+      return true;
+    } catch (error) {
+      if (error instanceof ExpiredTokenError) {
+        const refresh = JWT.refresh(token);
+
+        console.log('original', token, 'refreshed', refresh);
+
+        return CreateResponse.unauthorized('Token expired');
+      }
+
+      return CreateResponse.unauthorized('Invalid token');
+    }
+  }
+}
